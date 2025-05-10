4a871bced3833d38669217d4c37be97e75f03c3b
Soul <61690983+IgnoredSoul@users.noreply.github.com>
2025-05-10 14:00:02 +1000

    Merging Development branch to main. (#3)
    * Glorping time
    
    * erm
    
    * Create README.md
    
    * Read changed idk bro
    
    * Pain
    
    * I do not know what happened lol
    
    * gorp!?
    
    * wowie
    
    * GLORP!
    
    * Removed cock ;-;
    
    * Removed namespace and updates readme for it's release :3
    
    * README V1
    
    * Update README.md
    
    Touch up, fixing some stuff :3
    
    ---------
    
    Co-authored-by: Leetram519 <82403725+Leetram519@users.noreply.github.com>

---
+++ Main.cs
@@ -0,0 +1,429 @@
+﻿using Landfall.Haste;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using System.Reflection;
+using UnityEngine;
+using Zorro.Core;
+using static Landfall.Haste.ReactionUI;
+
+public enum Characters
+{
+	Captain,
+	Courier,
+	Keeper,
+	Leader,
+	Sage,
+	Heir,
+	Researcher,
+	Dalil,
+	Wraith,
+	Grunt,
+	Grunt2,
+	Grunt3,
+	Weeboh,
+	Weeboh2,
+	Weeboh3,
+}
+
+public struct ExtraConfig
+{
+	/// <summary>
+	/// Set's a custom camera rig for the NPC.
+	/// </summary>
+	public InteractionCameraRig cameraRig;
+
+	/// <summary>
+	/// Invoke action when the NPC dialog is finished
+	/// </summary>
+	public Action<NPC> onComplete;
+
+	/// <summary>
+	/// Invoke action when the NPC is created.
+	/// </summary>
+	public Action<NPC> onCreated;
+
+	public ExtraConfig()
+	{
+		cameraRig = null!;
+		onComplete = null!;
+		onCreated = null!;
+	}
+}
+
+public struct InterCharacter
+{
+	internal InterCharacter(Characters character, InteractionCharacter interactonCharacter)
+	{
+		Character = character;
+		Name = character switch
+		{
+			Characters.Captain => "The Captain",
+			Characters.Courier => "Zoe",
+			Characters.Keeper => "Riza",
+			Characters.Leader => "Ava",
+			Characters.Sage => "Daro",
+			Characters.Heir => "Niada",
+			Characters.Researcher => "Gan",
+			Characters.Wraith => "Wraith",
+			Characters.Dalil => "Dalil",
+			Characters.Grunt or Characters.Grunt2 or Characters.Grunt3 => "Grunt",
+			Characters.Weeboh or Characters.Weeboh2 or Characters.Weeboh3 => "Weeboh",
+			_ => "The Captain",
+		};
+
+		InteractionCharacter = interactonCharacter;
+		InteractionCharacter.CharacterSprite = GetCharacterSprite();
+		InteractionCharacter.VocalBank = GetCharacterVocals(Name);
+	}
+
+	/// <summary>
+	/// Save the character enum that is used in the dialog.
+	/// </summary>
+	public Characters Character { get; set; } = default;
+
+	/// <summary>
+	/// Creates the <seealso cref="InteractionCharacter"/> that is used in the dialog.
+	/// </summary>
+	public InteractionCharacter InteractionCharacter { get; set; } = null!;
+
+	/// <summary>
+	/// The name of the character that is used in the dialog.
+	/// </summary>
+	public string Name { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The <seealso cref="InteractionVocalBank"/> that the character uses.
+	/// </summary>
+	public InteractionVocalBank VocalBank { get; set; } = null!;
+
+	private Sprite GetCharacterSprite()
+	{
+		return GameObject.Find($"GAME/Handlers/InteractionHandler/InteractionUI/Canvas/Characters/{Character.ToString()}/Image")
+			.GetComponent<UnityEngine.UI.Image>().sprite;
+	}
+
+	private InteractionVocalBank GetCharacterVocals(string name)
+	{
+		return Resources.FindObjectsOfTypeAll<InteractionVocalBank>()
+			.FirstOrDefault(v => v.name.Contains(name, StringComparison.CurrentCultureIgnoreCase)) ?? null!;
+	}
+}
+
+public static class ILPatching
+{
+	/// <summary>
+	/// Overrides the Start and Awake methods of the <seealso cref="InteractableCharacter"/> class that is inherited from the <seealso cref="MonoBehaviour"/> class."
+	/// </summary>
+	/// <exception cref="Exception"></exception>
+	public static void InteractableCharacter_Patch()
+	{
+		// Get references to the methods and fields we need
+		MethodInfo startMethod = typeof(InteractableCharacter).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+		if (startMethod == null) throw new Exception("Couldn't find Start method");
+
+		MethodInfo awakeMethod = typeof(InteractableCharacter).GetMethod("Awake", BindingFlags.NonPublic | BindingFlags.Instance);
+		if (awakeMethod == null) throw new Exception("Couldn't find Awake method");
+
+		// Tell the original lines to fuck off and insert our own method call
+		new ILHook(awakeMethod, il =>
+		{
+			// Ee errrr
+			ILCursor c = new ILCursor(il);
+
+			// Move to first instruction
+			c.Goto(0);
+
+			// Remove all instructions
+			while (c.Next != null) c.Remove();
+
+			// Load 'this' (the InteractableCharacter instance)
+			c.Emit(OpCodes.Ldarg_0);
+
+			// Call method with "this" and false as arguments
+			c.Emit(OpCodes.Call, typeof(ILPatching).GetMethod("ReImp_Awake", BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance));
+
+			// Return, end of method
+			c.Emit(OpCodes.Ret);
+		});
+
+		// Insert our ReImp at the start, avoids rewriting the whole start method since we only need a check, then continue.
+		new ILHook(startMethod, il =>
+		{
+			// Ee errrr
+			ILCursor c = new ILCursor(il);
+
+			// Move to first instruction
+			c.Goto(0);
+
+			// Load 'this' (the InteractableCharacter instance)
+			c.Emit(OpCodes.Ldarg_0);
+
+			// Call method with "this" as argument
+			c.Emit(OpCodes.Call, typeof(ILPatching).GetMethod("ReImp_Start", BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance));
+		});
+	}
+
+	public static void ReImp_Awake(InteractableCharacter instance)
+	{
+		// This is because the default npc's have their shit setup in the unity hierarchy
+		// So if the instance does not have a questionMarkTarget, we return
+		if (!instance?.questionMarkTarget) { return; }
+
+		// Default code from InteractableCharacter
+		instance!.State = new InteractableCharacter.StateMachine(instance.questionMarkTarget, instance);
+		instance.State.RegisterState(new InteractableCharacter.NoneState());
+		instance.State.RegisterState(new InteractableCharacter.HasInteractionState());
+		instance.State.RegisterState(new InteractableCharacter.HasAbilityUnlockState());
+		instance.State.SwitchState<InteractableCharacter.NoneState>(false);
+	}
+
+	public static void ReImp_Start(InteractableCharacter instance) => ReImp_Awake(instance.State == null ? instance : null!);
+}
+
+/// <summary>
+/// A builder class for constructing dialog sequences associated with an NPC.
+/// Automatically commits the dialog to the NPC when disposed.
+/// </summary>
+public class DialogBuilder : List<DialogEntry>, IDisposable
+{
+	private readonly NPC _npc;
+
+	public DialogBuilder(NPC npc) => _npc = npc;
+
+	/// <summary>
+	/// Adds a new dialog entry to the list.
+	/// </summary>
+	/// <param name="c"></param>
+	/// <param name="l"></param>
+	public void Add(Characters c, string l) => Add(new DialogEntry(c, l));
+
+	/// <summary>
+	/// Calls the <see cref="NPC.CommitDialog"/> method once the using is finished.
+	/// Making it simplier for the user to create a dialog.
+	/// </summary>
+	public void Dispose() => _npc.CommitDialog(this);
+}
+
+/// <summary>
+/// Represents a non-player character (NPC) with an associated <seealso cref="Interaction"/>, <seealso cref="DialogBuilder"/> system, and <seealso cref="InteractableCharacter"/> logic.<br/>
+/// Supports multiple constructor overloads for flexibility in setup, and manages its own lifecycle within a static list of instances.
+/// </summary>
+public class NPC
+{
+	/// <summary>
+	/// Initializes a new NPC instance by validating inputs, setting up the <seealso cref="Interaction"/>,
+	/// attaching an <seealso cref="InteractableCharacter"/> component to the character, and storing optional configuration.
+	/// </summary>
+	/// <param name="character">The character transform to attach the interaction to.</param>
+	/// <param name="markerPoint">The marker point for the interaction's question mark UI.</param>
+	/// <param name="interactionName">A unique name for the interaction.</param>
+	/// <param name="exc">Optional extra configuration for the interaction setup.</param>
+	/// <exception cref="ArgumentNullException">Thrown if character is null.</exception>
+	/// <exception cref="ArgumentException">Thrown if interactionName is null, empty, or already used.</exception>
+	public NPC(Transform character, Transform markerPoint, string interactionName, ExtraConfig exc = default)
+	{
+		if (character == null)
+		{ throw new ArgumentNullException(nameof(character), "Character is null."); }
+
+		if (string.IsNullOrEmpty(interactionName))
+		{ throw new ArgumentException("Interaction name is null or empty.", nameof(interactionName)); }
+
+		if (instances.Any(i => i.interaction.name == interactionName))
+		{ throw new ArgumentException($"Interaction name '{interactionName}' already exists.", nameof(interactionName)); }
+
+		if (exc.cameraRig == null)
+		{ exc.cameraRig = GameObject.Find("Hub_Characters/CaptainCameraRig").GetComponent<InteractionCameraRig>(); }
+
+		// Setup the new interaction
+		interaction = new();
+		interaction.name = interactionName;
+		interaction.factsToSet = [];
+		interaction.canBePlayedSeveralTimes = true;
+
+		// Create a new InteractableCharacter component and add it to the Character gameobject.
+		// Additionally we turn it off straight away so the Start() method does not execute.
+		(interactionCharacter = character.gameObject.AddComponent<InteractableCharacter>()).enabled = false;
+		interactionCharacter.questionMarkTarget = markerPoint;
+		interactionCharacter.interactionCenter = character;
+		interactionCharacter.unlockInteraction = interaction;
+		interactionCharacter.cameraRig = exc.cameraRig;
+		interactionCharacter.onComplete = (exc.onComplete != null ? () => { exc.onComplete?.Invoke(this); } : null);
+
+		_extraConfig = exc;
+		characters = new();
+		instances.Add(this);
+	}
+
+	/// <summary>
+	/// Initializes a new NPC instance by validating inputs, setting up the <seealso cref="Interaction"/>,
+	/// attaching an <seealso cref="InteractableCharacter"/> component to the character, and storing optional configuration.
+	/// </summary>
+	/// <param name="character">The character GameObject to attach the interaction to.</param>
+	/// <param name="markerPoint">The marker point for the interaction's question mark UI.</param>
+	/// <param name="interactionName">A unique name for the interaction.</param>
+	/// <param name="exc">Optional extra configuration for the interaction setup.</param>
+	/// <exception cref="ArgumentNullException">Thrown if character is null.</exception>
+	/// <exception cref="ArgumentException">Thrown if interactionName is null, empty, or already used.</exception>
+	public NPC(GameObject character, Transform markerPoint, string interactionName, ExtraConfig exc = default) :
+		this(character.transform, markerPoint, interactionName, exc)
+	{ }
+
+	/// <summary>
+	/// Initializes a new NPC instance by validating inputs, setting up the <seealso cref="Interaction"/>,
+	/// attaching an <seealso cref="InteractableCharacter"/> component to the character, and storing optional configuration.
+	/// </summary>
+	/// <param name="character">The character transform to attach the interaction to.</param>
+	/// <param name="markerPoint">The marker point for the interaction's question mark UI.</param>
+	/// <param name="interactionName">A unique name for the interaction.</param>
+	/// <param name="exc">Optional extra configuration for the interaction setup.</param>
+	/// <exception cref="ArgumentNullException">Thrown if character is null.</exception>
+	/// <exception cref="ArgumentException">Thrown if interactionName is null, empty, or already used.</exception>
+	public NPC(Transform character, GameObject markerPoint, string interactionName, ExtraConfig exc = default) :
+		this(character, markerPoint.transform, interactionName, exc)
+	{ }
+
+	/// <summary>
+	/// Initializes a new NPC instance by validating inputs, setting up the <seealso cref="Interaction"/>,
+	/// attaching an <seealso cref="InteractableCharacter"/> component to the character, and storing optional configuration.
+	/// </summary>
+	/// <param name="character">The character Gameobject to attach the interaction to.</param>
+	/// <param name="markerPoint">The marker point for the interaction's question mark UI.</param>
+	/// <param name="interactionName">A unique name for the interaction.</param>
+	/// <param name="exc">Optional extra configuration for the interaction setup.</param>
+	/// <exception cref="ArgumentNullException">Thrown if character is null.</exception>
+	/// <exception cref="ArgumentException">Thrown if interactionName is null, empty, or already used.</exception>
+	public NPC(GameObject character, GameObject markerPoint, string interactionName, ExtraConfig exc = default) :
+		this(character.transform, markerPoint.transform, interactionName, exc)
+	{ }
+
+	/// <summary>
+	/// Initializes a new NPC instance by validating inputs, setting up the <seealso cref="Interaction"/>,
+	/// attaching an <seealso cref="InteractableCharacter"/> component to the character, and storing optional configuration.
+	/// </summary>
+	/// <param name="character">The character GameObject to attach the interaction to.</param>
+	/// <param name="markerOffset">Creates a new marker with an offset.</param>
+	/// <param name="interactionName">A unique name for the interaction.</param>
+	/// <param name="exc">Optional extra configuration for the interaction setup.</param>
+	/// <exception cref="ArgumentNullException">Thrown if character is null.</exception>
+	/// <exception cref="ArgumentException">Thrown if interactionName is null, empty, or already used.</exception>
+	public NPC(GameObject character, Vector3 markerOffset, string interactionName, ExtraConfig exc = default) :
+		this(character.transform, (GameObject)null!, interactionName, exc)
+	{
+		GameObject markerPoint = new GameObject("MarkerPoint");
+		markerPoint.transform.SetParent(character.transform);
+		markerPoint.transform.localPosition = markerOffset;
+		interactionCharacter.questionMarkTarget = markerPoint.transform;
+	}
+
+	/// <summary>
+	/// Contains a list of every NPC.
+	/// </summary>
+	public static List<NPC> instances { get; private set; } = new();
+
+	/// <summary>
+	/// Contains a list of every character that is used in the dialog.
+	/// </summary>
+	public List<InterCharacter> characters { get; set; } = null!;
+
+	/// <summary>
+	/// The Interaction that is created for the NPC.
+	/// </summary>
+	public Interaction interaction { get; set; } = null!;
+
+	/// <summary>
+	/// The InteractableCharacter component that is attached to the NPC.
+	/// </summary>
+	public InteractableCharacter interactionCharacter { get; set; } = null!;
+
+	/// <summary>
+	/// Contains every line that the dialog uses.
+	/// </summary>
+	public List<InteractionLine> lines { get; set; } = null!;
+
+	/// <summary>
+	/// Stores extra configuration for the NPC.
+	/// </summary>
+	private ExtraConfig _extraConfig { get; set; } = default;
+
+	/// <summary>
+	/// Commits the dialog to the NPC by doing checks, <seealso cref="InterCharacter"/> creation/obtaining and dialog validation.<br/>
+	/// This also clears any previous dialogs that were commited.
+	/// </summary>
+	/// <param name="dialogs"></param>
+	/// <exception cref="ArgumentException"></exception>
+	public void CommitDialog(List<DialogEntry> dialogs)
+	{
+		// Null check
+		if (dialogs == null) throw new ArgumentException("No dialog was provided.");
+
+		// Creates a new empty list, discarding the original lines
+		lines = new();
+
+		// Loop through each dialog entry
+		foreach (DialogEntry dialog in dialogs)
+		{
+			// Check if the line is null or empty
+			if (string.IsNullOrEmpty(dialog.line))
+			{
+				Debug.LogError($"Line: {dialog.line} is null or empty. Skipping line.");
+				return;
+			}
+
+			// If the dialog character is not valid somehow, skip the line
+			if (!Enum.IsDefined(typeof(Characters), dialog.character))
+			{
+				try { Debug.LogError($"Character: '{dialog.character.ToString()}' is invalid. Skipping line."); }
+				catch { Debug.LogError($"Character you've put in your dialog is invalid. Skipping line."); }
+				return;
+			}
+
+			// Get character by name
+			InterCharacter character = characters.FirstOrDefault(c => c.Character == dialog.character);
+
+			// If the character does not exist, we create a new one
+			if (character.InteractionCharacter == null || string.IsNullOrEmpty(character.Name))
+			{
+				InteractionCharacter intactChar = new();
+				characters.Add(new(dialog.character, intactChar));
+				character = characters.Last();
+
+				intactChar.DisplayName = new UnlocalizedString(character.Name);
+				intactChar.CharacterColor = new Color(0.3216f, 0.1137f, 0.2923f, 1); // Does not matter
+				intactChar.TalkingSprite = GameObject.Find("Hub_Characters/Captain/").GetComponent<InteractableCharacter>().character.TalkingSprite;  // Does not matter
+				intactChar.Ability = AbilityKind.BoardBoost; // Does not matter
+			}
+
+			InteractionLine interactionLine = new();
+			interactionLine.line = new UnlocalizedString(dialog.line);
+			interactionLine.character = character.InteractionCharacter;
+			interactionLine.requirements = [];
+			lines!.Add(interactionLine);
+			interaction.Lines = lines.ToArray();
+		}
+
+		interactionCharacter.character = lines.First().character;
+
+		if (!interactionCharacter.enabled)
+		{
+			interactionCharacter.enabled = true;
+			_extraConfig.onCreated?.Invoke(this);
+		}
+	}
+}
+
+[Landfall.Modding.LandfallPlugin]
+public class NPCLib
+{
+	// Do IL patching right when the mod registers.
+	// This should not affect any other mods since the class only starts when player is in the Hub.
+	static NPCLib()
+	{
+		UnityEngine.Debug.Log("[NPCLib]: Initializing IL Patching");
+		ILPatching.InteractableCharacter_Patch();
+		UnityEngine.Debug.Log("[NPCLib]: Finished IL Patching");
+		UnityEngine.Debug.LogWarning("[NPCLib]: Good luck!");
+	}
+}
+
+public record DialogEntry(Characters character, string line);
---
+++ README.md
@@ -0,0 +1,238 @@
+### **NPCLib**
+
+> [!CAUTION]
+> This library isn't fully tested and/or working. Expect things to break.
+> Contact `ignoredsoul` on Discord for more information.
+
+NPCLib is a library that helps with adding interaction dialogues to your custom NPCs.  
+
+This library contains all the resources you need to make your NPCs come to life, 
+whether you're making a witty sage, a quirky merchant, or just a grunt who says "ugh."
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **What does NPCLib do?**
+
+NPCLib is a modding tool that helps you:
+- Give your custom NPCs interaction dialogs.
+- Handle NPC interactions, behaviors, character sprites, vocal banks, and dialogue sequences with ease.
+- Patch Haste's `InteractableCharacter` class to make it work with your custom NPCs.
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **Features**
+
+- **Dialog System**:  
+  Use the `DialogBuilder` to create and oversee dialog sequences with an easy-to-use syntax.
+
+- **Custom Characters**:  
+  Define your NPCs using the `Characters` enum, and then let NPCLib take care of the rest, including vocal banks and sprites.
+
+- **Interaction System**:  
+  With a few lines of code, you can give your NPCs unique camera setups using the `InteractionCameraRig` class.
+
+- **IL Patching**:  
+  NPCLib patches the `InteractableCharacter` class, allowing you to concentrate on your mod without worrying about breaking anything.
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **Installation**
+
+To install this library:
+
+1. Clone this repository into your mod directory
+2. Edit your .csproj file.
+3. Add the following in your xml:
+```xml
+<ItemGroup>
+  <Compile Include=".\NPCLib\*.cs">
+    <Link>NPCLib\%(Filename)%(Extension)</Link>
+  </Compile>
+</ItemGroup>
+```
+
+> [!NOTE]
+> You can also directly add this library's code into your project.
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **How to use NPCLib in your project**
+
+#### Step 1: Create an NPC
+
+You can either create an `MarkerPoint` `GameObject` on your model for the marker to position itself
+```cs
+GameObject character = GameObject.Instantiate(MyCustomNPC);
+Transform markerPoint = character.transform.Find("MarkerPoint");
+NPC npc = new NPC(character, markerPoint, "MyUniqueInteractionName");
+```
+Or create an offset from the models center position. In this case, 5 units above the model.
+```cs
+GameObject character = GameObject.Instantiate(MyCustomNPC);
+NPC npc = new NPC(character, new Vector3(0, 5, 0), "MyUniqueInteractionName");
+```
+
+---
+<!--------------------------------------------------------------------------------------->
+
+#### Step 2: Add Dialog
+
+There are multiple ways of creating dialog for your NPC.
+```cs
+NPC npc = ...; // Your NPC instance
+using (new DialogBuilder(npc)
+{
+  { Characters.Captain, "Hello." },
+  { Characters.Courier, "Hi!" },
+}) { }
+```
+```cs
+NPC npc = ...; // Your NPC instance
+using (DialogBuilder builder = new DialogBuilder(npc))
+{
+  builder.Add(Characters.Captain, "Welcome to the ship!");
+  builder.Add(Characters.Courier, "I have a message for you.");
+}
+```
+```cs
+NPC npc = ...; // Your NPC instance
+DialogBuilder dialog = new DialogBuilder(npc);
+dialog.Add(Characters.Captain, "Hello.");
+dialog.Add(Characters.Courier, "Hi!");
+npc.CommitDialog(dialog);
+```
+```cs
+NPC npc = ...; // Your NPC instance
+List<DialogEntry> dialog = new List<DialogEntry>()
+{
+  new(Characters.Captain, "Hello."),
+  new(Characters.Courier, "Hi!")
+};
+npc.CommitDialog(dialog);
+```
+
+> [!TIP]
+> You can choose whichever approach works the best for you and your needs.
+
+---
+<!--------------------------------------------------------------------------------------->
+
+#### Step 3: Customize Behavior (Optional)
+
+Want to tweak how your NPC behaves? Use the `ExtraConfig` struct to add custom camera rigs or actions:
+```cs
+ExtraConfig config = new ExtraConfig
+{
+  cameraRig = customCameraRig,
+  onComplete = npc => Debug.Log("Dialog finished!"),
+  onCreated = npc => Debug.Log("NPC created!")
+};
+NPC npc = new NPC(character, markerPoint, "CustomInteraction", config);
+```
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **FAQ**
+
+#### **What is this for?**
+NPCLib is for modders of **Haste** who want to add interaction dialogs to their custom NPCs.</br>
+If you're making a mod with NPCs, this is for you... Seriously just use this rather than messing around yourself 😭
+
+#### **Does it work with other mods?**
+Most likely. This may cause issues with other similar libraries, but there are none for now.
+
+#### **What if I break something?**
+No worries! Open an issue or tell me in the [Haste Modding Community](https://discord.gg/hastebrokenworlds) server.
+
+
+### **Contributing**
+
+Got ideas? Found a bug? Want to add something cool? Fork the repo, make your changes, and send a pull request. Contributions are always welcome! </br>
+Seriously, this project is hell.
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **License**
+
+This project is licensed under the MIT License. Do whatever you want with it, just don’t blame me if shit breaks. </br>
+But if possible, just add me to some sort of credits 😭🙏
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **Shoutouts**
+
+Big thanks to:
+- **Steve** for bug testing, giving me pointers and keeping me sane. ily pookie.
+- **[Hamunii](https://github.com/hamunii)** for teaching me more about VisualStudio xml shit.
+
+---
+<!--------------------------------------------------------------------------------------->
+
+### **Final Words**
+```
+  ___   _   _    _  _____ _____   _______     _______ ______   _______ _   _ ___ _   _  ____ 
+ |_ _| | | | |  / \|_   _| ____| | ____\ \   / / ____|  _ \ \ / /_   _| | | |_ _| \ | |/ ___|
+  | |  | |_| | / _ \ | | |  _|   |  _|  \ \ / /|  _| | |_) \ V /  | | | |_| || ||  \| | |  _ 
+  | |  |  _  |/ ___ \| | | |___  | |___  \ V / | |___|  _ < | |   | | |  _  || || |\  | |_| |
+ |___| |_| |_/_/   \_\_| |_____| |_____|  \_/  |_____|_| \_\|_|   |_| |_| |_|___|_| \_|\____|
+                                                                                             
+```
+
+---
+<!--------------------------------------------------------------------------------------->
+
+</br>
+<h3>Anyway, here's the update journey...</h3></br>
+</br>
+
+### Update 1.0.0
+- Official release! 🥳🥳🥳
+- Updated README to be more informative.
+- Still need to rework the ILPatching to check and handle multiple people using the library. Will fix when I can be bothered :3
+
+### Update 0.2.1
+- Added more XML documentation.
+- Minor cleaning.
+
+### Update 0.2
+- Added an extra config struct.
+- Added summaries 
+- Reworked the InterCharacter class.
+- Changed most methods and fields to be public.
+
+### Update 0.1.2
+- I hate patches.
+
+### Update 0.1.1
+- Fixed Weeboh and Grunt's voicelines not being found cause I am stupid. <sup><sub>Thank you Stevelion for the catch</sub></sup>
+
+### Update 0.1
+- Added expressions enum.
+- Added new constructor for marker point offset instead of a transform.
+- Added actions for when the dialog is finished.
+- Added field to get the question mark gameobject.
+- Changed the dialog commit, now clearing the previous lines before adding the new dialog.
+- Changed and fixed instances field to get set method.
+- Changed other stuff, i forgor 💀
+- Removed TextTagParser patch, allowing expressions.
+
+<!--
+
+This will remain in "beta" for the time being. </br>
+Strictly for teasing and contributing till we have a fully working system that meets a few unlisted criteria. </br>
+
+(HOW THE FUCK DID I MANAGE TO WRITE "teasing" INSTEAD OF "testing" WHAT)
+
+Mainly, importing custom characters. </br>
+It's not necessarily hard to do, it's just trying to find a way to make it easier for the end-user. </br>
+
+But feel free to mess around with it. Break it. Re-write it. Spit on it. Don't care. Go for gold. </br>
+And if you want to use this, give credit where it's due and prepare for any bugs that will occur.
+-->
